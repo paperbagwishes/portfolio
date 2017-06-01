204e43132c28ef5c0df5b62ab052b60320ca2fe2
paperbagwishes <mmurphy15@it-mmc16-m14.eastern.ewu.edu>
2017-06-01 13:35:25 -0700

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
-// Scripts
-// Store Banner Images
-var img0 = 'https://unsplash.it/600/300?image=1';
-var img1 = 'https://unsplash.it/600/300?image=2';
-var img2 = 'https://unsplash.it/600/300?image=3';
 
-// Random Number Generator
-var maxRand = 3;
-var randNum = Math.floor( Math.random() * maxRand );
-console.log(randNum);
-
-// Swap Out Banner
-$('header img').attr('src', eval('img' + randNum) );
 
 // Lightgallery
 
@@ -21,5 +9,3 @@ $('figure').lightGallery({
 	counter: false
 
 });
-// Random Background
-$('body').addClass( 'bg' + randNum );

aa6ff44d5339f04b353aa543611b132575dbd3c3
paperbagwishes <mmurphy15@it-mmc16-m15.eastern.ewu.edu>
2017-05-31 20:37:08 -0700

    save

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,13 @@ console.log(randNum);
 $('header img').attr('src', eval('img' + randNum) );
 
 // Lightgallery
-$('figure').lightGallery({
-    selector: 'a', 
-    mode: 'lg-lollipop',
-    thumbnail: true
-});
 
+$('figure').lightGallery({
+  selector: 'a',
+  mode: 'lg-slide-circular',
+	speed: 1000,
+	counter: false
+
+});
 // Random Background
 $('body').addClass( 'bg' + randNum );
